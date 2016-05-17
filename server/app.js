2a283401742171e878500837c1af68a339add862
ujjwal <uforujjwal@yahoo.com>
2016-05-17 12:05:50 +0530

    front end

--- server/app.js
+++ server/app.js
@@ -6,30 +6,66 @@ import path from 'path'
 import pollController from './controllers/pollController'
 import voteController from './controllers/voteController'
 import bodyParser from 'body-parser'
+import compression from 'compression'
 
-async function connect () { 
+import webpack from 'webpack'
+import webpackMiddleware from 'webpack-dev-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
+import config from './../webpack.config.js'
+
+const compiler = webpack(config)
+const middleware = webpackMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+  contentBase: 'src',
+  stats: {
+    colors: true,
+    hash: false,
+    timings: true,
+    chunks: false,
+    chunkModules: false,
+    modules: false
+  }
+})
+
+async function connect () {
   db.discover = path.join(__dirname, 'models')
   db.matcher = function shouldImportModel (modelFileName) {
     return true
-  } 
-  await db.connect('andvote_schema', 'root', 'root',{force:false})
+  }
+  await db.connect('andvote_schema', 'root', 'root', {
+    force: false
+  })
 }
 
 (async function () {
   try {
     await connect()
   } catch (err) {
-    console.log(`An error occured when connecting: ${err}`)
+    console.log(chalk.blue(`An error occured when connecting: ${err}`))
   }
   const app = express()
   app.use(bodyParser.json())
   app.post('/api/poll', pollController.handlePost)
   app.get('/api/poll/:pollId', pollController.handleGet)
   app.post('/api/vote', voteController.handlePost)
+
   app.use(errorHandler)
 
+  const isDeveloping = process.env.NODE_ENV !== 'production'
+  if (isDeveloping) {
+    app.use(middleware)
+    app.use(webpackHotMiddleware(compiler))
+    app.get('*', function response (req, res) {
+      res.write(middleware.fileSystem.readFileSync(path.join(__dirname, '../dist/index.html')))
+      res.end()
+    })
+  } else {
+    app.use(compression())
+    app.use(express.static(path.join(__dirname, '../dist')))
+    app.get('*', function response (req, res) {
+      res.sendFile(path.join(__dirname, '../dist/index.html'))
+    })
+  }
   const port = 3000
   app.listen(port, () => console.log(`Running on port ${port}`))
 })()
-
-
